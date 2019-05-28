761638cff774192cfb8c01816486046d91ba54ec
scpepper69 <secret.crunch.pepper@gmail.com>
2019-05-28 14:47:50 +0900

    modified

--- stock_auto/app/app/stock_auto.py
+++ stock_auto/app/app/stock_auto.py
@@ -15,11 +15,18 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 from werkzeug import secure_filename
 app = Flask(__name__)
 
+UPLOAD_FOLDER = './result'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
 def index():
     return render_template('index.html', img_url='../../learning/result.png')
 
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 if __name__ == '__main__':
     app.debug = True
-    app.run(debug=False, host='0.0.0.0', port=8004)
+    app.run(debug=False, host='0.0.0.0', port=8041)
 
